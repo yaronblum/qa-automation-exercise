569117a8c192075477f81918851e1d7168c7692f
orweitzer <orw@connecteam.com>
2022-04-18 15:52:09 +0300

    test changes

--- README.md
+++ README.md
@@ -1,19 +1,11 @@
 # Pymobitest
 
-use python3.9
-- git clone [https://github.com/Connecteam/qa-automation-exercise.git](https://github.com/https://github.com/Connecteam/qa-automation-exercise.git)
+use python 3.9 or 3.10
+
+- git
+  clone [https://github.com/Connecteam/qa-automation-exercise.git](https://github.com/https://github.com/Connecteam/qa-automation-exercise.git)
 - cd qa-automation-exercise
 - python3 -m venv qa_test
 - source qa_test/bin/activate
 - pip install -r requirements.txt
 - pytest
-
-Login Credentials
-
-- link: https://app.connecteam.com/#/LoginOld
-- username: yonatan+828272728@connecteam.com
-- password: 1234
-
-in order to get upto date cookeis:
- - inpsect ->application-> cookies 
- - copy the session and _spirit cookies into the pytest script
--- conftest.py
+++ conftest.py
@@ -1,30 +1,13 @@
-import os
-from typing import List
-
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.webdriver import WebDriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.remote.webelement import WebElement
-from selenium.webdriver.support import expected_conditions
-from selenium.webdriver.support.wait import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 
 
-@pytest.fixture(scope="class")
-def chrome_driver_init(request):
+@pytest.fixture(autouse=True)
+def chrome_web_driver_base(request):
     options = webdriver.ChromeOptions()
-    options.add_argument("--start-maximized")
-    options.add_argument("--kiosk")
-    if "SELENIUM_HUB_URL" in os.environ:
-        chrome_driver = webdriver.Remote(
-            command_executor=os.environ["SELENIUM_HUB_URL"],
-            options=options,
-        )
-    else:
-        chrome_driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options)
-    request.cls.driver = chrome_driver
-    yield
+    chrome_driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options)
+    chrome_driver.delete_all_cookies()
+    chrome_driver.maximize_window()
+    yield chrome_driver
     chrome_driver.close()
-
-
